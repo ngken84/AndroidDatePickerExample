7836ca47a677d2c1128e1b052bc3b28cfeca464f
Ken Ng <ng.ken84@gmail.com>
2018-09-24 20:11:26 -0700

    Implement Simple Date Picker Dialog

--- app/src/main/java/joker/persona/ngrocken/androiddatepickerexample/MainActivity.java
+++ app/src/main/java/joker/persona/ngrocken/androiddatepickerexample/MainActivity.java
@@ -1,13 +1,56 @@
 package joker.persona.ngrocken.androiddatepickerexample;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatePickerDialog.OnDateSetListener mDatePickerDialog;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.main_text);
+        button = findViewById(R.id.main_btn_pick);
+
+        mDatePickerDialog = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                textView.setText("Date = " + month + "/" + day + "/" + year);
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDatePickerDialog, year, month, day);
+
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
     }
 }
